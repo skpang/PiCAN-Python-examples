e6fbd0253dbd1b738e929ae24d895142252b068f
SUKKIN PANG <sukkinpang@27-iMac-Fusion.local>
2016-02-01 20:45:54 +0000

    Inital release

---
+++ simple_rx_test.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+#
+# simple_rx_test.py
+# 
+# This python3 program logs all CAN messages to the sd card.
+# For use with PiCAN boards on the Raspberry Pi
+# http://skpang.co.uk/catalog/pican2-canbus-board-for-raspberry-pi-2-p-1475.html
+#
+# Make sure Python-CAN is installed first http://skpang.co.uk/blog/archives/1220
+#
+# 01-02-16 SK Pang
+#
+#
+#
+
+import RPi.GPIO as GPIO
+import can
+import time
+import os
+
+
+print('\n\rCAN Rx test')
+print('Bring up CAN0....')
+os.system("sudo /sbin/ip link set can0 up type can bitrate 500000")
+time.sleep(0.1)	
+
+try:
+	bus = can.interface.Bus(channel='can0', bustype='socketcan_native')
+except OSError:
+	print('Cannot find PiCAN board.')
+	exit()
+	
+print('Ready')
+
+try:
+	while True:
+		message = bus.recv()	# Wait until a message is received.
+		
+		c = '{0:f} {1:x} {2:x} '.format(message.timestamp, message.arbitration_id, message.dlc)
+		s=''
+		for i in range(message.dlc ):
+			s +=  '{0:x} '.format(message.data[i])
+			
+		print(' {}'.format(c+s))
+	
+	
+except KeyboardInterrupt:
+	#Catch keyboard interrupt
+	os.system("sudo /sbin/ip link set can0 down")
+	print('\n\rKeyboard interrtupt')	
